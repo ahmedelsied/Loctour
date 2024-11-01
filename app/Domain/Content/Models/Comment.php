18dd751719f62c114c8a3425b4f2ab7e52754d10
ahmed elsied <43114893+ahmedelsied@users.noreply.github.com>
2024-11-01 19:26:09 +0000

    Add comment model relationships

--- app/Domain/Content/Models/Comment.php
+++ app/Domain/Content/Models/Comment.php
@@ -5,7 +5,8 @@ namespace App\Domain\Content\Models;
 use App\Support\Concerns\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Domain\Core\Models\User;;
 class Comment extends Model
 {
     use HasFactory;
@@ -14,5 +15,20 @@ class Comment extends Model
     public function likes(): MorphMany
     {
         return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
     }
 }
